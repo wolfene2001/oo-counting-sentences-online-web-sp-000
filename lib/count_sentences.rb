bbe04f9c231dcaa59a585ef92bed3a70a734cb3b
wolfene2001 <wolfene2001@yahoo.com>
2019-12-30 10:38:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,12 +11,20 @@ class String
 end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
   end
+end
 
   def exclamation?
-
+    if self.end_with?(".")
+      return true
+    else
+      return false
   end
+end
 
   def count_sentences
 
